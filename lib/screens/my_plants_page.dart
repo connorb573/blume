e5deeeb47c20f7dac6921526f751cbc358d80d0e
Connor Brookes <connor.brookes@student.manchester.ac.uk>
2020-05-18 18:34:27 +0100

    Added ListView for plant cards

--- lib/screens/my_plants_page.dart
+++ lib/screens/my_plants_page.dart
@@ -5,6 +5,7 @@ import 'package:blume/modules/plant.dart';
 class MyPlants extends StatelessWidget {
   List<Plant> plants = [
     Plant(nickname: 'Pothos', waterLevel: 2, waterWeeks: 1),
+    Plant(nickname: 'terry', waterLevel: 1, waterWeeks: 3),
   ];
 
   @override
@@ -14,8 +15,11 @@ class MyPlants extends StatelessWidget {
       body: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: <Widget>[
-          Padding(
-            padding: EdgeInsets.only(top: 50, left: 20),
+          FlatButton(
+            onPressed: () {
+              Navigator.pop(context);
+            },
+            padding: EdgeInsets.only(top: 40),
             child: Icon(
               CupertinoIcons.back,
               size: 30,
@@ -33,44 +37,97 @@ class MyPlants extends StatelessWidget {
             ),
           ),
           SizedBox(height: 20),
-          Padding(
-            padding: EdgeInsets.all(20),
-            child: Container(
-              height: 100,
-              width: 375,
-              child: Row(
-                children: <Widget>[
-                  Padding(
-                    padding: EdgeInsets.only(
-                      left: 30,
-                    ),
-                    child: Text(
-                      plants[0].nickname,
-                      textAlign: TextAlign.start,
-                      style: TextStyle(
-                        fontSize: 20,
-                      ),
-                    ),
-                  ),
-                ],
+          Expanded(
+            child: ListView.builder(
+              itemCount: plants.length,
+              scrollDirection: Axis.vertical,
+              shrinkWrap: true,
+              itemBuilder: (context, index) {
+                final currentPlant = plants[index];
+                return PlantItem(plant: plants[index]);
+              },
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class PlantItem extends StatelessWidget {
+  const PlantItem({
+    @required this.plant,
+  });
+
+  final Plant plant;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.all(20),
+      child: Container(
+        height: 100,
+        width: 375,
+        child: Row(
+          children: <Widget>[
+            Padding(
+              padding: EdgeInsets.only(
+                left: 30,
               ),
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.all(
-                  Radius.circular(30),
+              child: Text(
+                plant.nickname,
+                textAlign: TextAlign.start,
+                style: TextStyle(
+                  fontSize: 20,
                 ),
-                color: Colors.white,
-                boxShadow: [
-                  BoxShadow(
-                    color: Colors.grey.withOpacity(0.7),
-                    spreadRadius: 3,
-                    blurRadius: 5,
-                    offset: Offset(0, 3),
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.only(top: 20, left: 80),
+              child: Column(
+                children: <Widget>[
+                  Text('How much?'),
+                  SizedBox(height: 5),
+                  Row(
+                    children: <Widget>[
+                      //Image from pixelperfect at www.flaticon.com
+                      Image.asset('images/dropcolour.png',
+                          height: 30, width: 20),
+                      //Image from Good Ware at www.flaticon.com
+                      Image.asset('images/dropbw.png', height: 30, width: 20),
+                      Image.asset('images/dropbw.png', height: 30, width: 20),
+                    ],
                   ),
                 ],
               ),
             ),
+            Expanded(
+              child: Padding(
+                padding: EdgeInsets.only(top: 30, left: 40),
+                child: Column(
+                  children: <Widget>[
+                    Text('Once'),
+                    Text('a week'),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.all(
+            Radius.circular(30),
           ),
-        ],
+          color: Colors.white,
+          boxShadow: [
+            BoxShadow(
+              color: Colors.grey.withOpacity(0.7),
+              spreadRadius: 3,
+              blurRadius: 5,
+              offset: Offset(0, 3),
+            ),
+          ],
+        ),
       ),
     );
   }
